70a9d7f9be33d6bc0e1b31247248e0528d6fb12e
Diego Moreira Sardinha de Souza <40876675+diegosou4@users.noreply.github.com>
2025-06-05 11:17:58 +0000

    (add) add funcionalitys to client, i need create a vector for channel and logic

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,16 +1,20 @@
 #include "Client.hpp"
+#include <ctime>
 
 
 Client::~Client(){
     std::cout << "Default Destructor" << std::endl;
 }
 
-Client::Client() : _client_fd(-1) , _Nick_name("As velinhas da Ricksom") {
+Client::Client() : _client_fd(-1) , _Nick_name(""), _isRegisted(false), _Second_choice(""), _Third_choice(""), _User_name(""), _Ip_address(""),_haslogout(false), _Last_login(""), _Password(""){
     std::cout << "Default Constuctor" << std::endl;
 }
 
-void Client::setClientFd(int fd)
-{
+void Client::setRegisted(){
+    _isRegisted = true;
+}
+
+void Client::setClientFd(int fd){
     _client_fd = fd;
 }
 
@@ -20,6 +24,19 @@ void Client::setIpAddress(std::string ipaddress){
 
 void Client::setNickName(std::string nick_name){
     _Nick_name = nick_name;
+}
+
+void Client::SetLastLogin()
+{
+    time_t now = time(0);
+    struct tm* tstruct = localtime(&now);
+    char buffer[20];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", tstruct);
+    _Last_login = buffer;
+}
+
+void Client::SetPassword(std::string pass){
+    _Password = pass;
 }
 
 int Client::getClientFd() const{
@@ -32,4 +49,27 @@ std::string Client::getIpAddress() const{
 
 std::string Client::getNickName() const{
     return _Nick_name;
+}
+
+
+std::string Client::getLastLogin() const{
+    return _Last_login;
+}
+
+std::string Client::getPassword() const {
+    return _Password;
+}
+
+
+void Client::WelcomeToIrc() const{
+    std::cout << "Welcome " << _User_name << "to Amazing Irc Server" << std::endl;
+    if(_haslogout == true)
+    std::cout << "Your last login" << getLastLogin() << std::endl;
+}
+
+
+void Client::userInfo() const{
+    std::cout << "Nick name:" << _Nick_name << std::endl;
+    std::cout << "User name:" << _User_name << std::endl;
+    std::cout << "Ip:" << _Ip_address << std::endl;
 }
--- src/Client.hpp
+++ src/Client.hpp
@@ -8,23 +8,43 @@
 class Client {
     private : 
         int _client_fd;
+        bool _isRegisted;
+        bool _haslogout;
+        std::string _Ip_address;
+
         std::string _Nick_name;
         std::string _Second_choice;
         std::string _Third_choice;
         std::string _User_name;
-        std::string _Ip_address;
+        std::string _Password;
+        std::string  _Last_login;
+
         // Lista de canais que o cliente esta vinculado
     public:
         ~Client();
         Client();
+
+        // Setters
         void setClientFd(int fd);
         void setIpAddress(std::string ipaddress);
         void setNickName(std::string nick_name);
-        
+        void setRegisted();
+        void SetLastLogin();
+        void SetPassword(std::string pass);
+    
+
+        // Getters
 
         std::string getNickName() const;
         int getClientFd() const;
         std::string getIpAddress() const;
+        std::string getLastLogin() const;
+        std::string getPassword() const;
+
+    
+        // Utils Fuctions
+        void WelcomeToIrc() const;
+        void userInfo() const;
 
 
 };
