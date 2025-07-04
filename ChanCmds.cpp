9db66e5bfc99eafb12d1e73096f7e02bc210c9ea
Camille <camille-bouvet@hotmail.fr>
2025-07-04 19:12:31 +0100

    fix Part

--- ChanCmds.cpp
+++ ChanCmds.cpp
@@ -6,7 +6,7 @@
 /*   By: cbouvet <cbouvet@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 21:35:58 by cbouvet           #+#    #+#             */
-/*   Updated: 2025/07/04 17:40:17 by cbouvet          ###   ########.fr       */
+/*   Updated: 2025/07/04 19:12:04 by cbouvet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,17 +108,17 @@ void Server::cmdPart(Client *client, Channel *channel, str_vector const &msg)
 	else
 		code = cmdCheck(client, channel, "");
 
-	if (!code)
-		code = channel->removeMember(*client);
-
 	if (code)
 		return (this->sendNumeric(*client, channel, code));
 
-	if (!channel->isEmpty())
-		return (this->broadcastAll(*client, *channel, msg[0], this->argExists(msg, 2)));
+	this->broadcastAll(*client, *channel, msg[0], channel->getName() + " " + this->argExists(msg, 2));
+	channel->removeMember(*client);
 
-	this->_channels.erase(channel->getName());
-	delete channel;
+	if (channel->isEmpty())
+	{
+		this->_channels.erase(channel->getName());
+		delete channel;
+	}
 }
 
 // Performs checks, sends name list & appropriate codes
